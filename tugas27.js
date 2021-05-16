ae2be2965ab64c508d75f0f1c46965fd2834d9c2
Faza <firdausauliafaza@gmail.com>
2021-05-16 18:20:58 +0700

    niomic.com

---
+++ tugas27.js
@@ -0,0 +1,16 @@
+function kata1() {
+  let a = "No ";
+  let b = "rumah saya ";
+  let c = 30;
+  let d = "A ";
+  let x = a + b + String(c) + d;
+  console.log(x);
+}
+kata1();
+function kata2() {
+  let e = "Jalan pangeran Tirtayasa No ";
+  let f = 70;
+  let z = e + String(f);
+  console.log(z);
+}
+kata2();
